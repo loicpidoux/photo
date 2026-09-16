1ec2c034bd864225d63424e5ab96a1e02abdb455
loicpidoux <pidouxloic@ik.me>
2026-09-16 23:12:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ function resizeScratchCanvas() {
 resizeScratchCanvas();
 window.addEventListener('resize', resizeScratchCanvas);
 
-const SCRATCH_SKIP_CHANCE = 0.5;
+const SCRATCH_SKIP_CHANCE = 0.3;
 const SCRATCH_MAX_OPACITY = 0.03;
 const SCRATCH_LINE_WIDTH = 1;
 const SCRATCH_FADE_MS = 250;
