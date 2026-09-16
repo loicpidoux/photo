2dda8c9dc03e527b8d11b5d510b0b8cc7e0e85ed
loicpidoux <pidouxloic@ik.me>
2026-09-16 19:31:03 +0200

    Refactor image viewer functionality in script.js

--- script.js
+++ script.js
@@ -174,9 +174,9 @@ resizeScratchCanvas();
 window.addEventListener('resize', resizeScratchCanvas);
 
 const SCRATCH_SKIP_CHANCE = 0.5;
-const SCRATCH_MAX_OPACITY = 0.9;
+const SCRATCH_MAX_OPACITY = 0.03;
 const SCRATCH_LINE_WIDTH = 1;
-const SCRATCH_FADE_MS = 350;
+const SCRATCH_FADE_MS = 250;
 
 let scratchLastX = null;
 let scratchLastY = null;
