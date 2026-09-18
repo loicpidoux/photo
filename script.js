acd010f22aede19b0c25b4cb37d217c06c5e9bce
loicpidoux <pidouxloic@ik.me>
2026-09-18 09:26:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ fetch('/scratch')
   })
   .catch(() => {});
 
-const SCRATCH_SKIP_CHANCE = 0.4;
+const SCRATCH_SKIP_CHANCE = 0.9;
 const SCRATCH_MAX_OPACITY = 0.2;
 const SCRATCH_LINE_WIDTH = 1;
 const SCRATCH_FADE_MS = 60;
