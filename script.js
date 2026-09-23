753da6b65c6c80ef08d5c4f770cf505a0fc533a5
loicpidoux <pidouxloic@ik.me>
2026-09-23 15:57:01 +0200

    Refactor script.js for improved functionality

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const FRAME_REF_W = 1920;
 const FRAME_REF_H = 1140;
 const referenceFrame = document.getElementById('referenceFrame');
 
+const MOBILE_ZOOM_FACTOR = 1.3;
+
 function updateFrameTransform() {
   let scale = Math.max(screen.width / FRAME_REF_W, screen.height / FRAME_REF_H);
   if (isMobileDevice()) {
